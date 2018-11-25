bec23b51288cdf57d2999b4d6f45495629bfc2c7
Nicolas Mahe <nicolas@mahe.me>
2018-11-25 13:52:11 +0700

    Improve polling

--- src/newBlock/polling.ts
+++ src/newBlock/polling.ts
@@ -3,11 +3,14 @@ import { NewBlockEventEmitterInterface } from "./interface"
 
 export = async (bitcoinClient: any, blockConfirmations: number, initialBlockNumber: number | null, pollingTime: number): Promise<NewBlockEventEmitterInterface> => {
   const newBlock: NewBlockEventEmitterInterface = new EventEmitter()
-  let previousBlockNumber: number = initialBlockNumber || -1
+  let previousBlockNumber = initialBlockNumber
   const pollingBlockNumber = async () => {
     try {
       const latestBlockNumber = await bitcoinClient.getBlockCount()
       const lastBlockNumber = latestBlockNumber - blockConfirmations
+      if (previousBlockNumber === null) {
+        previousBlockNumber = lastBlockNumber
+      }
       for (let blockNumber = previousBlockNumber + 1; blockNumber <= lastBlockNumber; blockNumber++) {
         const blockHash = await bitcoinClient.getBlockHash(blockNumber)
         newBlock.emit('newBlock', {
